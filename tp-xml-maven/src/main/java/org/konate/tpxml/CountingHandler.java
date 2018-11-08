edc4f9b692759ecea4145c38ffe6b214404b0189
11408560 <11408560@tel10-f200-6.ad.ig-edu.univ-paris13.fr>
2018-11-08 15:06:35 +0100

    Set up maven and dom4j dependencies

---
+++ tp-xml-maven/src/main/java/org/konate/tpxml/CountingHandler.java
@@ -0,0 +1,23 @@
+package org.konate.tpxml;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class CountingHandler extends DefaultHandler {
+	private int count;
+	
+	@Override
+	public void startElement(String uri, String localName,
+            				 String qName, Attributes attributes) {
+		count++;
+	}
+
+	/**
+	 * @return the count
+	 */
+	public int getCount() {
+		return count;
+	}
+	
+	
+}

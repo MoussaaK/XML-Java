20ae1a240627e6eb2677b628ef024f722967977a
Moussee <moussakonate27@gmail.com>
2018-11-09 18:12:06 +0100

    Updated XMLUtil

--- tp-xml-maven/src/main/java/org/konate/tpxml/exo1/XMLUtil.java
+++ tp-xml-maven/src/main/java/org/konate/tpxml/exo1/XMLUtil.java
@@ -5,14 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.List;
 
 import org.dom4j.Document;
-import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.io.OutputFormat;
-import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 import org.xml.sax.helpers.DefaultHandler;
 
